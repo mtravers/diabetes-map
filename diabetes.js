25a9cff30ce5ae4b873713e6775034a0983e2387
Mike Travers <mt@collaborativedrug.com>
2013-03-26 21:22:04 -0700

    rename some files for sanity

--- diabetes.js
+++ diabetes.js
@@ -1,25 +1,99 @@
-queue()
-    .defer(d3.json, "data/us-states.json")
-//    .defer(d3.json, "data/diabetes1.json")
-    .await(ready);
+var width = 960,
+    height = 500,
+    centered;
 
 var path = d3.geo.path();
 
 var svg = d3.select("body").append("svg")
-    .attr("width", 400)
-    .attr("height", 300);
+    .attr("width", width)
+    .attr("height", height);
 
-var map = svg.append("g")
-    .attr("class", "map");
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", click);
 
-function ready(error, us, data) {
-    map
-	.selectAll("path")
-    	.data(topojson.object(us, us.objects.states).geometries)
+var g = svg.append("g")
+    .attr("id", "states");
+
+
+queue()
+    .defer(d3.json, "data/us-states1.json")
+    .defer(d3.csv, "data/title v table d3.csv")
+    .await(ready);
+
+// I'd rather roll this data into the geo data, but couldn't figure the right js magic for that.
+var titlev;
+
+function ready(error, us, titlev_a) {
+    titlev = titlev_a;
+    g.selectAll("path")
+	.data(us.features)
 	.enter().append("path")
-//    	.attr("class", "states")
-    	.attr("d", path);
+	.attr("d", path)
+	.attr("class", function(d) { 
+	    var row = state2row(d);
+	    return row == null ? "no" : "yes";
+	})
+	.on("click", click);
+};
 
+function state2row(dstate) {
+    var abbrev = dstate["abbrev"];
+    var rows = titlev.filter(function(r) { return  r["abbrev"] == abbrev;});
+    return rows.length == 1 ? rows[0] : null;
+}
+
+function click(d) {
+  var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
+
+  g.selectAll("path")
+	.classed("active", centered && function(d) { return d === centered; })
+	.sort(function (a, b) { 
+	    if (centered && a.id != centered.id) return -1;  
+	    else return 1;   
+	    });
+
+  g.transition()
+      .duration(1000)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
+
+    updateText(centered);
 
 }
+
+function updateText(stateData) {
+    setField('stitle', stateData['name']); 
+    var row = state2row(stateData);
+    if (row == null) {
+	d3.select('#desc').style("display", "none");
+    }
+    else {
+	d3.select('#desc').style("display", "");
+	d3.select('#desc').style("display", "");
+
+	setField('max_eligibility_age', row['Maximum Age']);
+    }
+
+}
+
+function setField(name, text) {
+    document.getElementById(name).innerHTML = text;
+}
+
 
---
+++ notes/git-workflow
@@ -0,0 +1,1 @@
+http://lea.verou.me/2011/10/easily-keep-gh-pages-in-sync-with-master/
