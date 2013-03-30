3bfda1de6f7165f668f907ad0a350c8fa08bcc76
Mike Travers <mt@collaborativedrug.com>
2013-03-30 14:44:31 -0700

    more tracking

--- diabetes-track.js
+++ diabetes-track.js
@@ -1,4 +1,4 @@
-var scale = .8,
+var scale = 1.0,
     width = 960 * scale,
     height = 500 * scale;
 
@@ -61,10 +61,10 @@ function move(d) {
 	});
 
     updateText(d);
-    // +++ move text box to mouse pos
     var box = document.getElementById('desc');
     var map = document.getElementById('map');
     var mouse = d3.mouse(map);
+    box.style.display = '';
     box.style.left = mouse[0] + "px";
     box.style.top = mouse[1] + "px";
 }
